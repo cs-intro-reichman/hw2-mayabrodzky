44831f32087270b71236e93e8dd27413ec5008c1
Maya Brodzky <maya.brodzky@post.runi.ac.il>
2025-11-12 19:36:47 +0200

    Completed HW2 assignment

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,29 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String word = args[0];
+            int times = Integer.parseInt(args[1]);
+
+            word = word.toUpperCase();
+
+            String anLetters = "AEFHILMNORSX";
+
+            for (int i=0; i<word.length(); i++) {
+                char letter = word.charAt(i);
+
+                String article = "a";
+
+                if (anLetters.indexOf(letter) >= 0) {
+                        article = "an";
+                }
+
+                System.out.println("Give me " + article + " " + letter + ": " + letter + "!");
+            }
+
+            System.out.println("What does that spell?");
+            for (int i=0; i<times; i++) {
+                System.out.println(word + "!!!");
+            }
+
         }
 }
